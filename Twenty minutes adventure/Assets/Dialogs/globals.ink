00448c3dd3d5cf315cb775bbe4acf26137f1c630
Ri <99540995+illirie@users.noreply.github.com>
2022-05-09 15:50:54 +0400

    added some variables in ink

--- Twenty minutes adventure/Assets/Dialogs/globals.ink
+++ Twenty minutes adventure/Assets/Dialogs/globals.ink
@@ -1,1 +1,5 @@
 VAR call_accepted = false
+VAR partner_was_met = false
+VAR body_investigated = false
+VAR Stepanida_told_everything = false
+VAR Stepanida_refused = false
--- Twenty minutes adventure/Assets/Scripts/SettingsScript.cs
+++ Twenty minutes adventure/Assets/Scripts/SettingsScript.cs
@@ -8,27 +8,27 @@ using UnityEngine.UI;
 public class SettingsScript : MonoBehaviour
 {
     public AudioMixer Am;
-    Resolution[] rsl;
-    List<string> resolutions;
-    public Dropdown dropdown;
-    bool isFullScreen = true;
+    //Resolution[] rsl;
+    //List<string> resolutions;
+    //public Dropdown dropdown;
+    //bool isFullScreen = true;
 
-    public void Awake()
-    {
-        resolutions = new List<string>();
-        rsl = Screen.resolutions;
-        foreach (var i in rsl)
-        {
-            resolutions.Add(i.width + "x" + i.height);
-        }
-        dropdown.ClearOptions();
-        dropdown.AddOptions(resolutions);
-    }
+    //public void Awake()
+    //{
+    //    resolutions = new List<string>();
+    //    rsl = Screen.resolutions;
+    //    foreach (var i in rsl)
+    //    {
+    //        resolutions.Add(i.width + "x" + i.height);
+    //    }
+    //    dropdown.ClearOptions();
+    //    dropdown.AddOptions(resolutions);
+    //}
 
-    public void Resolution(int r)
-    {
-        Screen.SetResolution(rsl[r].width, rsl[r].height, isFullScreen);
-    }
+    //public void Resolution(int r)
+    //{
+    //    Screen.SetResolution(rsl[r].width, rsl[r].height, isFullScreen);
+    //}
 
     public void BackToMenuPressed()
     {
