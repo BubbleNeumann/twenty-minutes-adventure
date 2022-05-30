608be06038d7ea8a65e48c86bfa749cc94c89603
BubbleNeumann <dana@chkgo.com>
2022-05-30 15:02:41 +0400

    attempt to add credits

--- Twenty minutes adventure/Assets/Scripts/DialogTrigger.cs
+++ Twenty minutes adventure/Assets/Scripts/DialogTrigger.cs
@@ -4,21 +4,36 @@ using UnityEngine;
 using DialogWindow;
 using UnityEngine.SceneManagement;
 using Ink.Runtime;
+// using static VideoPlayer.VideoPlayer;
+// using System;
+// using System.Windows.Forms;
 
 namespace DialogTrigger
 {
     public class DialogTrigger : MonoBehaviour
     {
-        private bool playerInRange;
+        private bool playerInRange = false;
 
         [SerializeField] private bool objectIsTrigger;
-
         [Header("InkJSON")]
         [SerializeField] private TextAsset inkJSON;
+        // private GameObject player;
+        private UnityEngine.Video.VideoPlayer videoPlayer;
 
-        private void Awake()
+
+        void Start()
         {
-            playerInRange = false;
+            try
+            {
+                videoPlayer = GameObject.Find("Video Player").GetComponent<UnityEngine.Video.VideoPlayer>();
+                videoPlayer.Pause();
+                videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.CameraFarPlane;
+            }
+            catch
+            {
+                
+            }
+
         }
 
         public void StartConversation()
@@ -33,6 +48,90 @@ namespace DialogTrigger
         {
             if (((Ink.Runtime.BoolValue)DialogManager.GetInstance().GetVariableState("donuts_chosen")).value || ((Ink.Runtime.BoolValue)DialogManager.GetInstance().GetVariableState("case_closed")).value)
             {
+                // var videoPlayer = GameObject.Find("Video Player").GetComponent<UnityEngine.Video.VideoPlayer>();
+                try
+                {
+                    videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.CameraNearPlane;
+                    // videoPlayer.frame = 1;
+                    videoPlayer.Play();
+                }
+                catch
+                {
+                    
+                }
+                
+                // while(videoPlayer.frame < 100)
+                // {
+                //     videoPlayer.Play();
+                //     videoPlayer.frame += 1;
+                //     videoPlayer.Pause();
+                // }
+               
+                // Destroy(videoPlayer, 10000);
+                // play credits cutscene
+            
+                // VideoPlayer.playCredits();
+                
+                // gameObject.GetComponent<VideoPlayer>().playCredits();
+                // VideoPlayer.playCredits();
+                // VideoPlayer player = new VideoPlayer();
+                // player.playCredits();
+                // Action foobar = () => foo.bar.foobar();
+                // Action playCredits = () => VideoPlayer.VideoPlayer.playCredits();
+                // GameObject camera = GameObject.Find("Main Camera");
+                
+                // camera.GetComponent<UnityEngine.Video.VideoPlayer>().Play();
+                // var videoPlayer = camera.GetComponent<UnityEngine.Video.VideoPlayer>();
+
+                // Debug.Log(videoPlayer);
+
+
+                // videoPlayer.url = "/Users/graham/movie.mov";
+                // videoPlayer.SetActive(true);
+                // videoPlayer.url = "Titry_3.mp4";
+                // videoPlayer.Play();
+                // videoPlayer.SetActive(true);
+                // GameObject player = GameObject.Find("Video Player");
+                // player.SetActive(true);
+                // Destroy(player, 10000);
+                // player.Play();
+
+                // GameObject camera = GameObject.Find("Main Camera");
+
+                // VideoPlayer automatically targets the camera backplane when it is added
+                // to a camera object, no need to change videoPlayer.targetCamera.
+                // var videoPlayer = camera.AddComponent<UnityEngine.Video.VideoPlayer>();
+
+                // Play on awake defaults to true. Set it to false to avoid the url set
+                // below to auto-start playback since we're in Start().
+                
+
+                // By default, VideoPlayers added to a camera will use the far plane.
+                // Let's target the near plane instead.
+                // videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.CameraNearPlane;
+
+                // This will cause our Scene to be visible through the video being played.
+                // videoPlayer.targetCameraAlpha = 0.5F;
+
+                // Set the video to play. URL supports local absolute or relative paths.
+                // Here, using absolute.
+                // videoPlayer.url = "file://C:/Users/bubbl/twenty-minutes-adventure/Twenty minutes adventure/Assets/Sprites/Titry_3.mp4";
+                // videoPlayer.playOnAwake = true;
+                // Skip the first 100 frames.
+                // videoPlayer.frame = 100;
+
+                // Restart from beginning when done.
+                // videoPlayer.isLooping = true;
+
+                // Each time we reach the end, we slow down the playback by a factor of 10.
+                // videoPlayer.loopPointReached += EndReached;
+
+                // Start playback. This means the VideoPlayer may have to prepare (reserve
+                // resources, pre-load a few frames, etc.). To better control the delays
+                // associated with this preparation one can use videoPlayer.Prepare() along with
+                // its prepareCompleted event.
+                // videoPlayer.Play();
+
                 SceneManager.LoadScene("Menu");
             }
         }
--- Twenty minutes adventure/Assets/Scripts/DiaryScript.cs
+++ Twenty minutes adventure/Assets/Scripts/DiaryScript.cs
@@ -7,21 +7,13 @@ public class DiaryScript : MonoBehaviour
 {
     [SerializeField] private GameObject DiaryCanvas;
     public static bool diaryIsOpen = false;
-    // Start is called before the first frame update
-    // void Start()
-    // {
-    //     DiaryCanvas.SetActive(false);
-    // }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.N))
         {
-            DiaryCanvas.SetActive(true);
-            // DiaryCanvas.transform.SetActive(true);
-            // diaryIsOpen = !diaryIsOpen;
-            // Debug.Log("here we go");
+            diaryIsOpen = !diaryIsOpen;
+            DiaryCanvas.SetActive(diaryIsOpen);
         }
     }
 }
